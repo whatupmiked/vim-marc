4ab09735fcc2f72801d0a6f0d5dc5d8c7daefabd
whatupmiked <whatupmiked@gmail.com>
2019-05-26 15:18:36 +0100

    Adding multi-record support

--- python/mmmarc.py
+++ python/mmmarc.py
@@ -1,35 +1,27 @@
 #!/bin/python3
 import vim
-import sys
-import argparse
 
-def mrcDisplay():
-    """
-    VIM Script function for converting a single line
-    mrc record into a displayed mrc record
-    """
-    record = vim.current.buffer[0]
-    split_record = record.replace('\x1f','$').replace('\x1d','\n').split('\x1e')
+def singleMrcDisplay(stdin):
+    displayed = ''
+    split_record = stdin.replace('\x1f','$').replace('\x1d','\n').split('\x1e')
     split_record = split_record[:-1]
     LDR = split_record[0][:24]
     recordDIR = split_record[0][24:]
-    vim.current.buffer[0] = "=LDR  " + LDR
+    displayed += "=LDR  " + LDR + "\n"
     directory = []
     j = 0
     for i in range(12, len(recordDIR)+12, 12):
         directory.append(recordDIR[j:i])
         j = i
-    # Print out the display record
+    # Build the display record
     for i in range(len(directory)):
-        vim.current.buffer.append("=" + directory[i][:3] + "  " + split_record[i+1])
+        displayed += "=" + directory[i][:3] + "  " + split_record[i+1] + "\n"
+    return displayed + "\n"
 
-def mrcCompile():
-    """
-    VIM Script function for converting a multi line
-    mrc record in display format to compile a compiled
-    record
-    """
-    display_list = vim.current.buffer
+def singleMrcCompile(stdin):
+    display_record = stdin
+    display_list = display_record.split('\n')
+    display_list.pop()
 
     field_len = []
     field_tag = []
@@ -39,6 +31,7 @@ def mrcCompile():
         field_len.append(len(display_list[i+1][5:].encode('utf-8')))
         field_tag.append(display_list[i+1][1:4])
 
+    field_len = field_len[:-1]
     total_offset = 0
     field_offset = []
     for i in range(len(field_len)):
@@ -60,11 +53,46 @@ def mrcCompile():
     field_delimiter = '\x1e' #^^
     subfield_delimiter = '\x1f' #^_
 
-    for k in range(len(display_list)-1):
-        compiled_record += display_list[k+1].replace('$', subfield_delimiter)[6:] + field_delimiter
+    for k in range(1, len(display_list)-1):
+        compiled_record += display_list[k].replace('$', subfield_delimiter)[6:] + field_delimiter
 
     compiled_record += eof_delimiter
     compiled_record = LDR + field_delimiter + compiled_record
 
+    return compiled_record
+
+def mrcDisplay():
+    stdin = vim.current.buffer[0]
+    records = []
+    displayedOut = ''
+    if stdin.count('\x1d') > 1:
+        # split stdin
+        records = stdin.replace('\x1d','\x1d\n').split('\n')
+    else:
+        records.append(stdin)
+    for rcd in records:
+        if rcd != '':
+            displayedOut += singleMrcDisplay(rcd)
     del vim.current.buffer[:]
-    vim.current.buffer[0] = compiled_record
+    # Split on new-line and return list since VIM
+    # buffer throws vim.error on strings with newline
+    displayedOutList = displayedOut.split("\n")
+    vim.current.buffer[0] = displayedOutList[0]
+    vim.current.buffer.append(displayedOutList[1:-1])
+
+def mrcCompile():
+    # Join on newline because vim buffers
+    # Strip newlines from lines
+    stdin = '\n'.join(vim.current.buffer) + '\n'
+    compiledOut = ''
+    if stdin.count('\n\n') > 1:
+        records = []
+        # split stdin
+        records = stdin.split('\n\n')
+        for rcd in records:
+            if rcd != '':
+                compiledOut += singleMrcCompile(rcd+'\n\n')
+    else:
+        compiledOut = singleMrcCompile(stdin)
+    del vim.current.buffer[:]
+    vim.current.buffer[0] = compiledOut
